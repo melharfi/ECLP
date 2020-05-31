79f25665bdd7df9daeef3495ec5621dddbbda28e
Mohssine EL HARFI <the-morpher@hotmail.com>
2020-05-31 15:38:59 +0000

    change to Regex pattern

--- ECLP/ECLP.cs
+++ ECLP/ECLP.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Dynamic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading;
 using System.Threading.Tasks;
 using The_Morpher.Events;
@@ -13,16 +14,6 @@ namespace The_Morpher
 {
     public class ECLP
     {
-        #region define Verbs
-        public string FlagsVerb = "-f";
-        public string MultuFlagsVerb = "-f-m";
-        public string PropertiesVerb = "-p";
-        public string MutiPropertiesVerb = "-p-m";
-        public string CollectionsVerb = "-c";
-        public string MultiCollectionsVerb = "-c-m";
-        public string ExCollectionsVerb = "-xc";
-        #endregion
-
         #region Events
         public event EventHandler<AddedArgsEvents> AddedArgs;
         public event EventHandler<AddedFlagsEvents> AddedFlags;
@@ -34,7 +25,7 @@ namespace The_Morpher
         /// <summary>
         /// Command as one signle string
         /// </summary>
-        public readonly string Raw;
+        public string Raw;
 
         /// <summary>
         /// String of arguments to ba parsed
@@ -52,237 +43,183 @@ namespace The_Morpher
         public ECLP(string[] args)
         {
             this.Raw = string.Join(" ", args);
+            this.Raw = this.Raw.Trim();
         }
-        public ArgsModel Parse()
+        public CommandResult Parse()
         {
-            ArgsModel data = new ArgsModel();
-            data.Clear();
-            string[] args = Raw.Split(' ');
+            CommandResult data = new CommandResult();
 
-            for (int i = 0; i < args.Count(); i++)
+            #region Flags verb ex pattern "--verbose --zomby --deadmatch"
             {
-                // define actual verb
-                // if no one of the known verbs is found then it's an Arg
-                if (args[i] != FlagsVerb && args[i] != MultuFlagsVerb && args[i] != PropertiesVerb && args[i] != MutiPropertiesVerb && args[i] != CollectionsVerb && args[i] != MultiCollectionsVerb && args[i] != ExCollectionsVerb)
+                #region Fetch for matches
+                string pattern = @"--\w\S+";
+                MatchCollection matches = Regex.Matches(Raw, pattern);
+                data.Flags = matches.Cast<Match>().Select(match => match.Value).ToList();
+                #endregion
+
+                #region TriggerEvent
+                foreach (string flag in data.Flags)
                 {
-                    // Args, ex "verbose 5 3,5 true --v"
-                    object detectedType = DetectType(args[i]);
-                    if (detectedType == null)
-                        detectedType = args[i];
-                    object newValue = Convert.ChangeType(args[i], detectedType.GetType());
-                    data.Args.Add(newValue);
+                    AddedFlagsEvents addedFlagsEvents = new AddedFlagsEvents
+                    {
+                        Flag = flag
+                    };
+                    OnAddedFlags(addedFlagsEvents);
+                }
+                #endregion
+
+                #region Cleaning Raw
+                Raw = Regex.Replace(Raw, pattern, "");      // deleting pattern just fetched
+                Raw = Regex.Replace(Raw, @" \s*", " ");     // deleting multispaces
+                this.Raw = this.Raw.Trim();                 // deleting single space from start or end
+                #endregion
+            }
+            #endregion
+
+            #region Property verb ex pattern "-p driver=steave -p age=30"
+            {
+                #region Fetch for matches
+                string pattern = @"-p (\w\S+)=(\w\S+)";
+                MatchCollection matches = Regex.Matches(Raw, pattern);
+                #endregion
+
+                #region raitement + Parsing values + Trigger Events
+                foreach (Match match in matches)
+                {
+                    var name = match.Groups[1].Value;
+                    var value = match.Groups[2].Value;
+                    object newValue = DetectType(value);
+                    data.Properties.Add(name, newValue);
 
                     #region TriggerEvent
-                    AddedArgsEvents aELPArgsEvents = new AddedArgsEvents
+                    AddedPropertiesEvents addedPropertiesEvents = new AddedPropertiesEvents
                     {
-                        Arg = newValue
+                        Property = new KeyValuePair<string, object>(name, newValue)
                     };
-                    OnAddedArgs(aELPArgsEvents);
+                    OnAddedProperties(addedPropertiesEvents);
                     #endregion
                 }
-                else
-                {
-                    // check if next argument exist otherwize no need to associate a null value
-                    if (args.Count() == i + 1)
-                        break;
+                #endregion
 
-                    if (args[i] == FlagsVerb)
+                #region Cleaning Raw
+                Raw = Regex.Replace(Raw, pattern, "");      // deleting pattern just fetched
+                Raw = Regex.Replace(Raw, @" \s*", " ");     // deleting multispaces
+                this.Raw = this.Raw.Trim();                 // deleting single space from start or end
+                #endregion
+            }
+            #endregion
+
+            #region Collection verb ex pattern "-c players=steave|john|clark -c ages=21|15|30"
+            {
+                #region Fetch for matches
+                string pattern = @"-c (\w\S+)=(\w\S+)\|?(\w\S+)?";
+                MatchCollection matches = Regex.Matches(Raw, pattern);
+                #endregion
+
+                #region Traitement + Parsing values + Trigger Events
+                foreach (Match match in matches)
+                {
+                    var name = match.Groups[1].Value;
+                    object[] values = new object[match.Groups[2].Value.Split('|').Count()];
+                    string[] oldValues = match.Groups[2].Value.Split('|');
+                    for (int v = 0; v < oldValues.Length; v++)
                     {
-                        // Flags, ex "-f Driver"
-                        string flag = args[i + 1];
-                        data.Flags.Add(flag);
+                        object newValue = DetectType(oldValues[v]);
+                        values[v] = newValue;
+                    }
+                    data.Collections.Add(name, values);
+
+                    #region TriggerEvent
+                    AddedCollectionsEvents addedCollectionsEvents = new AddedCollectionsEvents
+                    {
+                        Collections = new KeyValuePair<string, object[]>(name, values)
+                    };
+                    OnAddedCollections(addedCollectionsEvents);
+                    #endregion
+                }
+                #endregion
+
+                #region Cleaning Raw
+                Raw = Regex.Replace(Raw, pattern, "");      // deleting pattern just fetched
+                Raw = Regex.Replace(Raw, @" \s*", " ");     // deleting multispaces
+                this.Raw = this.Raw.Trim();                 // deleting single space from start or end
+                #endregion
+            }
+            #endregion
+
+            #region ExCollection verb ex pattern "-xc players=steave:21|john:15|clark:30 -xc adresses=Japan:Tokyo|USA:Washington"
+            {
+                #region Fetch for matches
+                string pattern = @"-xc (\w\S+)=(\w\S+:\w\S+[\|\w\S+:\w\S])?";
+                MatchCollection matches = Regex.Matches(Raw, pattern);
+                #endregion
+
+                #region Traitement + Parsing values + Trigger Events
+                foreach (Match match in matches)
+                {
+                    List<KeyValuePair<string, object>> l = new List<KeyValuePair<string, object>>();
+                    var name = match.Groups[1].Value;
+                    string[] rawValues = match.Groups[2].Value.Split('|');
+
+                    foreach (string values in rawValues)
+                    {
+                        string[] subValues = values.Split(':');
+                        if (subValues.Count() == 2)
+                        {
+                            string key = subValues[0];
+                            object newValue = DetectType(subValues[1]);
+                            KeyValuePair<string, object> value = new KeyValuePair<string, object>(key, newValue);
+                            l.Add(value);
+                        }
+                    }
+
+                    data.ExCollections.Add(name, l);
+
+                    #region TriggerEvent
+                    AddedExCollectionsEvents addedExCollectionsEvents = new AddedExCollectionsEvents();
+                    addedExCollectionsEvents.ExCollections = new KeyValuePair<string, List<KeyValuePair<string, object>>>(name, l);
+                    OnAddedExCollections(addedExCollectionsEvents);
+                    #endregion
+                }
+                #endregion
+
+                #region Cleaning Raw
+                Raw = Regex.Replace(Raw, pattern, "");      // deleting pattern just fetched
+                Raw = Regex.Replace(Raw, @" \s*", " ");     // deleting multispaces
+                this.Raw = this.Raw.Trim();                 // deleting single space from start or end
+                #endregion
+            }
+            #endregion
+
+            // Args should be last thing to fetch
+            #region Args verb ex "5 3,5 true james"
+            {
+                string[] args = Raw.Split(' ');
+                for (int i = 0; i < args.Count(); i++)
+                {
+                    // double check if it's not a verb just escape matching in the previouse code
+                    // if no one of the known verbs is found then it's an Arg
+                    if (args[i] != "--" && args[i] != "-p" && args[i] != "-c" && args[i] != "-xc")
+                    {
+                        // Args, ex "james 5 3,5 true"
+                        object detectedType = DetectType(args[i]);
+                        if (detectedType == null)
+                            detectedType = args[i];
+                        object newValue = Convert.ChangeType(args[i], detectedType.GetType());
+                        data.Args.Add(newValue);
 
                         #region TriggerEvent
-                        AddedFlagsEvents aELPFlagsEvents = new AddedFlagsEvents
+                        AddedArgsEvents aELPArgsEvents = new AddedArgsEvents
                         {
-                            Flag = flag
+                            Arg = newValue
                         };
-                        OnAddedFlags(aELPFlagsEvents);
-                        #endregion
-
-                        #region Skip next argument
-                        i++;
-                        if (args.Count() == i + 1)
-                            break;
-                        #endregion
-                    }
-                    else if(args[i] == MultuFlagsVerb)
-                    {
-                        // Multiple Flags, ex "-f-m driver,car,california"
-                        string[] flags = args[i + 1].Split('|');
-                        foreach (string flag in flags)
-                        {
-                            data.Flags.Add(flag);
-                            #region TriggerEvent
-                            AddedFlagsEvents aELPFlagsEvents = new AddedFlagsEvents
-                            {
-                                Flag = flag
-                            };
-                            OnAddedFlags(aELPFlagsEvents);
-                            #endregion
-                        }
-
-                        #region Skip next argument
-                        i++;
-                        if (args.Count() == i + 1)
-                            break;
-                        #endregion
-                    }
-                    else if (args[i] == PropertiesVerb)
-                    {
-                        // Properties, ex "-p driver=steave"
-                        string[] prop = args[i + 1].Split('=');
-                        if(prop.Count() == 2)
-                        {
-                            string name = prop[0];
-                            string value = prop[1];
-                            object newValue = DetectType(value);
-                            data.Properties.Add(name, newValue);
-
-                            #region TriggerEvent
-                            AddedPropertiesEvents aELPPropertiesEvents = new AddedPropertiesEvents();
-                            aELPPropertiesEvents.Property = new KeyValuePair<string, object>(name, newValue);
-                            OnAddedProperties(aELPPropertiesEvents);
-                            #endregion
-                        }
-
-                        #region Skip next argument
-                        i++;
-                        if (args.Count() == i + 1)
-                            break;
-                        #endregion
-                    }
-                    else if (args[i] == MutiPropertiesVerb)
-                    {
-                        // Properties, ex "-p-m driver=steave|age=30"
-                        string[] raws = args[i + 1].Split('|');
-                        foreach (string raw in raws)
-                        {
-                            string[] prop = raw.Split('=');
-                            if (prop.Count() == 2)
-                            {
-                                string name = prop[0];
-                                string value = prop[1];
-                                object newValue = DetectType(value);
-                                data.Properties.Add(name, newValue);
-
-                                #region TriggerEvent
-                                AddedPropertiesEvents aELPPropertiesEvents = new AddedPropertiesEvents();
-                                aELPPropertiesEvents.Property = new KeyValuePair<string, object>(name, newValue);
-                                OnAddedProperties(aELPPropertiesEvents);
-                                #endregion
-                            }
-                        }
-
-                        #region Skip next argument
-                        i++;
-                        if (args.Count() == i + 1)
-                            break;
-                        #endregion
-                    }
-                    else if (args[i] == CollectionsVerb)
-                    {
-                        // Properties, ex "-c players=steave|john|clark"
-                        string[] col = args[i + 1].Split('=');
-                        if (col.Count() == 2)
-                        {
-                            string name = col[0];
-                            object[] values = new object[col[1].Split('|').Count()];
-
-                            string[] oldValues = col[1].Split('|');
-                            for (int v = 0; v < oldValues.Length; v++)
-                            {
-                                object newValue = DetectType(oldValues[v]);
-                                values[v] = newValue;
-                            }
-                            data.Collections.Add(name, values);
-
-                            #region TriggerEvent
-                            AddedCollectionsEvents aELPCollectionsEvents = new AddedCollectionsEvents();
-                            aELPCollectionsEvents.Collections = new KeyValuePair<string, object[]>(name, values);
-                            OnAddedCollections(aELPCollectionsEvents);
-                            #endregion
-                        }
-
-                        #region Skip next argument
-                        i++;
-                        if (args.Count() == i + 1)
-                            break;
-                        #endregion
-                    }
-                    else if (args[i] == MultiCollectionsVerb)
-                    {
-                        // Properties, ex "-c-m players=steave|john|clark/ages=21|15|30"
-                        string[] raws = args[i + 1].Split('/');
-                        foreach(string raw in raws)
-                        {
-                            string[] col = raw.Split('=');
-                            if (col.Count() == 2)
-                            {
-                                string name = col[0];
-                                object[] values = new object[col[1].Split('|').Count()];
-
-                                string[] oldValues = col[1].Split('|');
-                                for (int v = 0; v < oldValues.Length; v++)
-                                {
-                                    object newValue = DetectType(oldValues[v]);
-                                    values[v] = newValue;
-                                }
-                                data.Collections.Add(name, values);
-
-                                #region TriggerEvent
-                                AddedCollectionsEvents aELPCollectionsEvents = new AddedCollectionsEvents();
-                                aELPCollectionsEvents.Collections = new KeyValuePair<string, object[]>(name, values);
-                                OnAddedCollections(aELPCollectionsEvents);
-                                #endregion
-                            }
-                        }
-
-                        #region Skip next argument
-                        i++;
-                        if (args.Count() == i + 1)
-                            break;
-                        #endregion
-                    }
-                    else if (args[i] == ExCollectionsVerb)
-                    {
-                        // Extanded Collection, ex "-xc players=steave:21|john:15|clark:30"
-                        string[] col = args[i + 1].Split('=');
-                        if (col.Count() == 2)
-                        {
-                            List<KeyValuePair<string, object>> l = new List<KeyValuePair<string, object>>();
-                            string name = col[0];
-                            string[] rawValues = col[1].Split('|');
-
-                            foreach (string values in rawValues)
-                            {
-                                string[] subValues = values.Split(':');
-                                if(subValues.Count() == 2)
-                                {
-                                    string key = subValues[0];
-                                    object newValue = DetectType(subValues[1]);
-                                    KeyValuePair<string, object> value = new KeyValuePair<string, object>(key, newValue);
-                                    l.Add(value);
-                                }
-                            }
-
-                            data.ExCollections.Add(name, l);
-
-                            #region TriggerEvent
-                            AddedExCollectionsEvents aELPExCollectionsEvents = new AddedExCollectionsEvents();
-                            aELPExCollectionsEvents.ExCollections = new KeyValuePair<string, List<KeyValuePair<string, object>>>(name, l);
-                            OnAddedExCollections(aELPExCollectionsEvents);
-                            #endregion
-                        }
-
-                        #region Skip next argument
-                        i++;
-                        if (args.Count() == i + 1)
-                            break;
+                        OnAddedArgs(aELPArgsEvents);
                         #endregion
                     }
                 }
             }
+            #endregion
+
             return data;
         }
         private object DetectType(string str)
